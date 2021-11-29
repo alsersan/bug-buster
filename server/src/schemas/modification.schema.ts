27ef9589ac8209e6bd8123b5c4418771f72369c6
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-29 16:12:26 +0100

    Add Modification schema

---
+++ server/src/schemas/modification.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+import { User } from './user.schema';
+
+export type ModificationDocument = Modification & Document;
+
+@Schema()
+export class Modification {
+  @Prop({ required: true })
+  modifiedProperty: string;
+
+  @Prop({ required: true })
+  oldValue: string;
+
+  @Prop({ required: true })
+  newValue: string;
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] })
+  author: User[];
+}
+
+export const ModificationSchema = SchemaFactory.createForClass(Modification);
