b18cfbb369557943788664c2705a6b94e002dcaa
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-29 23:19:41 +0100

    Add patch request for users

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { identity } from 'rxjs';
 import { UserDocument } from 'src/schemas/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -22,13 +21,13 @@ export class UsersService {
     return this.userModel.findOne({ _id: id });
   }
 
-  /* update(id: string, updateUserDto: UpdateUserDto) {
+  update(id: string, updateUserDto: UpdateUserDto) {
     return this.userModel.findByIdAndUpdate(id, updateUserDto, {
       new: true,
     });
-  } */
+  }
 
   remove(id: string) {
-    return this.userModel.deleteOne({ _id: id });
+    return this.userModel.findByIdAndDelete(id);
   }
 }
