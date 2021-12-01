565953e55b7cad04d948ea63a9b261749a527d3d
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-01 16:02:08 +0100

    Add dtos for projects

--- server/src/projects/dto/create-project.dto.ts
+++ server/src/projects/dto/create-project.dto.ts
@@ -1,1 +1,26 @@
-export class CreateProjectDto {}
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { Members } from '../schemas/members.schema';
+
+export class CreateProjectDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  status: string;
+
+  @IsNotEmpty()
+  @IsString()
+  dateStarted: string;
+
+  @Type(() => Members)
+  @IsOptional()
+  @IsNotEmpty()
+  members: Members;
+}
--- server/src/projects/dto/update-project.dto.ts
+++ server/src/projects/dto/update-project.dto.ts
@@ -1,4 +1,47 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { Exclude, Type } from 'class-transformer';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { Ticket } from 'src/tickets/ticket.schema';
+import { Members } from '../schemas/members.schema';
 import { CreateProjectDto } from './create-project.dto';
 
-export class UpdateProjectDto extends PartialType(CreateProjectDto) {}
+export class UpdateProjectDto extends PartialType(CreateProjectDto) {
+  @IsOptional()
+  @Exclude()
+  _id: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  status: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  dateStarted: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  dateEnded: string;
+
+  @Type(() => Ticket)
+  @IsOptional()
+  @IsNotEmpty()
+  tickets: Ticket[];
+
+  @Type(() => Members)
+  @IsOptional()
+  @IsNotEmpty()
+  members: Members;
+}
