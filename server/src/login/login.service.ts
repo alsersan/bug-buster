3553e4f2b9004806fa46408eac039fabddfabdf9
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-03 11:05:38 +0100

    Add login service and controller

--- server/src/login/login.service.ts
+++ server/src/login/login.service.ts
@@ -1,26 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { CreateLoginDto } from './dto/create-login.dto';
-import { UpdateLoginDto } from './dto/update-login.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { User, UserDocument } from 'src/users/schemas/user.schema';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class LoginService {
-  create(createLoginDto: CreateLoginDto) {
-    return 'This action adds a new login';
-  }
-
-  findAll() {
-    return `This action returns all login`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} login`;
-  }
-
-  update(id: number, updateLoginDto: UpdateLoginDto) {
-    return `This action updates a #${id} login`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} login`;
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  async loginUser(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const user = await this.userModel.findOne({ email });
+    if (!user) {
+      throw new Error('Invalid email or password');
+    }
+    const isCorrectPassword = await bcrypt.compare(password, user.password);
+    if (!isCorrectPassword) {
+      throw new Error('Invalid email or password');
+    }
   }
 }
