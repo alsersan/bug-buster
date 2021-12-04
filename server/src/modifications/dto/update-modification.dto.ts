8fc19cc8df609fca74d618a43d62d33970f8c210
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-04 17:54:37 +0100

    Remove unnecesary dto

--- server/src/modifications/dto/update-modification.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateModificationDto } from './create-modification.dto';
-
-export class UpdateModificationDto extends PartialType(CreateModificationDto) {}
