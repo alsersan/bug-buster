6c99145fcc77fbe5051174902ec9f14bb07ccd03
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-04 16:41:42 +0100

    Add tests for ModificationsController

--- server/src/modifications/modifications.controller.spec.ts
+++ server/src/modifications/modifications.controller.spec.ts
@@ -1,20 +1,64 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateModificationDto } from './dto/create-modification.dto';
 import { ModificationsController } from './modifications.controller';
 import { ModificationsService } from './modifications.service';
 
 describe('ModificationsController', () => {
   let controller: ModificationsController;
+  let service: ModificationsService;
+
+  const mockModification = {
+    _id: '1',
+    modifiedProperty: 'test',
+    oldValue: 'test',
+    newValue: 'test',
+    dateCreated: 'today',
+    author: '',
+    ticket: '',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ModificationsController],
-      providers: [ModificationsService],
+      providers: [
+        {
+          provide: ModificationsService,
+          useValue: {
+            createModification: jest.fn().mockResolvedValue(mockModification),
+            getAllModifications: jest
+              .fn()
+              .mockResolvedValue([mockModification]),
+            getModificationById: jest.fn().mockResolvedValue(mockModification),
+            updateModification: jest.fn().mockResolvedValue(mockModification),
+            deleteModification: jest.fn().mockResolvedValue({
+              deletedModificationId: mockModification._id,
+            }),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ModificationsController>(ModificationsController);
+    service = module.get<ModificationsService>(ModificationsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('when it is instantiated', () => {
+    test('then it should be defined', () => {
+      expect(controller).toBeDefined();
+    });
+  });
+
+  describe('when create is called', () => {
+    test('then ModificationsService.createModification should be called with a new modification', async () => {
+      const newModification = new CreateModificationDto();
+      await controller.create(newModification);
+      expect(service.createModification).toHaveBeenCalledWith(newModification);
+    });
+    test('then it should return a new modification', async () => {
+      const newModification = await controller.create(
+        new CreateModificationDto(),
+      );
+      expect(newModification).toEqual(mockModification);
+    });
   });
 });
