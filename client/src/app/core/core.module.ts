764dd4a4bd8a5fb5768ae8e8ab5feac5c3880edc
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-06 13:36:10 +0100

    Add sidebar

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout/layout.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [HeaderComponent, LayoutComponent],
+  declarations: [HeaderComponent, LayoutComponent, SidebarComponent],
   imports: [CommonModule],
   exports: [LayoutComponent],
 })
