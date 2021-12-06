f0a1f53800d614593e357c3d0f188e00af5ae717
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-06 16:46:19 +0100

    Add users service

---
+++ client/src/app/services/users/users.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  private usersUrl = 'http://localhost:3001/users';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(private http: HttpClient) {}
+
+  createUser(hero: User): Observable<User> {
+    return this.http.post<User>(this.usersUrl, hero, this.httpOptions);
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl);
+  }
+
+  getUserById(id: string): Observable<User> {
+    return this.http.get<User>(`${this.usersUrl}/${id}`);
+  }
+
+  updateUser(id: string, update: Partial<User>): Observable<User> {
+    return this.http.patch<User>(
+      `${this.usersUrl}/${id}`,
+      update,
+      this.httpOptions
+    );
+  }
+
+  deleteUser(id: string): Observable<User> {
+    return this.http.delete<User>(`${this.usersUrl}/${id}`, this.httpOptions);
+  }
+}
