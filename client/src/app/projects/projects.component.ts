c5d71d12d4d46d961e230fc77b5b88353ca3662c
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-07 16:39:23 +0100

    Add async pipe in projects

--- client/src/app/projects/projects.component.ts
+++ client/src/app/projects/projects.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ProjectsService } from '../services/projects/projects.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectsComponent implements OnInit {
-  projects: any[] = [];
+  projects$!: Observable<Array<any>>;
 
   constructor(private projectsService: ProjectsService) {}
 
   ngOnInit(): void {
-    this.projectsService
-      .getProjects()
-      .subscribe((projects) => (this.projects = projects));
+    this.projects$ = this.projectsService.getProjects();
   }
 }
