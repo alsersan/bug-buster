ac97c29bf1ee2b8639eb39f8c7ea69327b141568
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-07 16:05:20 +0100

    Refactor sidebar component

--- client/src/app/core/sidebar/sidebar.component.ts
+++ client/src/app/core/sidebar/sidebar.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  sections: string[] = ['dashboard', 'projects', 'users', 'profile'];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
