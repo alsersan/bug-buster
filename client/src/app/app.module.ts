371acb8455a69d59a8ad14c36c80a09d4e37c1c1
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-07 15:24:09 +0100

    Fetch projects in projects component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { AuthInterceptor } from './services/auth/auth.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- client/src/app/projects/projects.component.ts
+++ client/src/app/projects/projects.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from '../services/projects/projects.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  projects: any[] = [];
 
-  constructor() { }
+  constructor(private projectsService: ProjectsService) {}
 
   ngOnInit(): void {
+    this.projectsService
+      .getProjects()
+      .subscribe((projects) => (this.projects = projects));
   }
-
 }
