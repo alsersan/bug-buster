d29f001519f563d9a9626328aacc37d52f2fd1fa
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-07 12:26:54 +0100

    Add routing for dashboard module

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout/layout.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, LayoutComponent, SidebarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [LayoutComponent],
 })
 export class CoreModule {}
