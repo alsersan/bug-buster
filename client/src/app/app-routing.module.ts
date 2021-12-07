fc801bdca3a829e0bae3db0e4c1a50c19dcb99dc
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-07 12:30:47 +0100

    Add routing for projects module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
     loadChildren: () =>
       import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
   },
+  { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
 ];
 
 @NgModule({
