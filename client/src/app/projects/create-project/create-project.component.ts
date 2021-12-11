b7558ccadafa578375e55b470fd4b4b761a90ecd
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-12 00:52:24 +0100

    Add styles for add project

--- client/src/app/projects/create-project/create-project.component.ts
+++ client/src/app/projects/create-project/create-project.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { NewProject, Project } from 'src/app/models/project.model';
 import { createProject } from 'src/app/store/projects/projects.actions';
@@ -11,22 +10,38 @@ import { createProject } from 'src/app/store/projects/projects.actions';
   styleUrls: ['./create-project.component.scss'],
 })
 export class CreateProjectComponent {
-  createProject = new FormGroup({
-    name: new FormControl(''),
-    description: new FormControl(''),
-    dateCreated: new FormControl(''),
-    projectManager: new FormControl('61aa5d8510c9e6570de7332e'),
-  });
+  managers = [
+    { name: 'bla', _id: '2' },
+    { name: 'blu', _id: '3' },
+    {
+      name: '61aa5d8510c9e6570de7332e',
+      _id: '61aa5d8510c9e6570de7332e',
+      hidden: true,
+    },
+  ];
+  createProject = new FormGroup(
+    {
+      name: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      projectManager: new FormControl(
+        '61aa5d8510c9e6570de7332e',
+        Validators.required
+      ),
+    },
+    { updateOn: 'blur' }
+  );
   constructor(private store: Store<{ projects: Project[] }>) {}
 
   onSubmit() {
-    const formResult = this.createProject.value;
-    const project: NewProject = {
-      name: formResult.name,
-      description: formResult.description,
-      dateCreated: 'today',
-      members: { projectManager: formResult.projectManager },
-    };
-    this.store.dispatch(createProject({ project }));
+    if (this.createProject.valid) {
+      const formResult = this.createProject.value;
+      const project: NewProject = {
+        name: formResult.name,
+        description: formResult.description,
+        dateCreated: 'today',
+        members: { projectManager: formResult.projectManager },
+      };
+      this.store.dispatch(createProject({ project }));
+    }
   }
 }
