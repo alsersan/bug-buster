af3b654b77524fda71a749b13561f49b0a972df1
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-13 12:20:41 +0100

    Fix imports in user test

--- client/src/app/users/user/user.component.spec.ts
+++ client/src/app/users/user/user.component.spec.ts
@@ -1,16 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { UserComponent } from './user.component';
 
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
+  let initialState = { users: [] };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserComponent ]
-    })
-    .compileComponents();
+      declarations: [UserComponent],
+      imports: [RouterTestingModule],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
   });
 
   beforeEach(() => {
