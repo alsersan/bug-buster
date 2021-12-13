0f93287eaf50a0ab4a84f775115f18fd2ef8c302
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-13 16:41:04 +0100

    Complete test of ProjectComponent

--- client/src/app/projects/project/project.component.spec.ts
+++ client/src/app/projects/project/project.component.spec.ts
@@ -1,19 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
+import { MembersComponent } from '../members/members.component';
+import { ProjectDetailsComponent } from '../project-details/project-details.component';
+import { TicketsComponent } from '../tickets/tickets.component';
 
 import { ProjectComponent } from './project.component';
 
 describe('ProjectComponent', () => {
   let component: ProjectComponent;
   let fixture: ComponentFixture<ProjectComponent>;
-  let initialState = { projects: [] };
+  let initialState = {
+    projects: [
+      {
+        _id: '1',
+        name: 'test',
+        description: 'test',
+        dateCreated: new Date(),
+        dateClosed: null,
+        tickets: [],
+        members: {
+          projectManager: '',
+          developers: [],
+          qualityAssurance: [],
+        },
+      },
+    ],
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ProjectComponent],
+      declarations: [
+        ProjectComponent,
+        MembersComponent,
+        ProjectDetailsComponent,
+        TicketsComponent,
+      ],
       imports: [RouterTestingModule],
-      providers: [provideMockStore({ initialState })],
+      providers: [
+        provideMockStore({ initialState }),
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { paramMap: convertToParamMap({ id: '1' }) } },
+        },
+      ],
     }).compileComponents();
   });
 
