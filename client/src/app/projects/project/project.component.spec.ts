b02d0048a828e3b3db56e9799c1aa151bf72a575
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-13 12:17:40 +0100

    Fix imports in project test

--- client/src/app/projects/project/project.component.spec.ts
+++ client/src/app/projects/project/project.component.spec.ts
@@ -1,16 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { ProjectComponent } from './project.component';
 
 describe('ProjectComponent', () => {
   let component: ProjectComponent;
   let fixture: ComponentFixture<ProjectComponent>;
+  let initialState = { projects: [] };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProjectComponent ]
-    })
-    .compileComponents();
+      declarations: [ProjectComponent],
+      imports: [RouterTestingModule],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
   });
 
   beforeEach(() => {
