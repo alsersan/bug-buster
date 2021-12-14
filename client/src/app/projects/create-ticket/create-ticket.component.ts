7c90b3a39bb62d87ea79e38bc68df32883cfee77
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-14 19:40:23 +0100

    Style create ticket component

--- client/src/app/projects/create-ticket/create-ticket.component.ts
+++ client/src/app/projects/create-ticket/create-ticket.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Project } from 'src/app/models/project.model';
+import { UserState } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-create-ticket',
@@ -7,18 +10,52 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./create-ticket.component.scss'],
 })
 export class CreateTicketComponent implements OnInit {
+  @Input() project!: Project;
+  ticketTypes = [
+    { name: 'Bug', value: 'bug' },
+    { name: 'Feature request', value: 'feature-request' },
+    { name: 'Other', value: 'other' },
+  ];
+  ticketPriorities = [
+    { name: 'Low', value: 'low' },
+    { name: 'Medium', value: 'medium' },
+    { name: 'High', value: 'high' },
+    { name: 'Immediate', value: 'immediate' },
+  ];
   createTicket!: FormGroup;
-  constructor() {}
+  loguedInUser!: UserState;
+
+  constructor(private store: Store<{ loguedInUser: UserState }>) {}
 
   ngOnInit(): void {
     this.createTicket = new FormGroup(
       {
         name: new FormControl('', Validators.required),
         description: new FormControl('', Validators.required),
+        type: new FormControl('', Validators.required),
+        priority: new FormControl('', Validators.required),
       },
       { updateOn: 'blur' }
     );
+
+    this.store
+      .select('loguedInUser')
+      .subscribe((user) => (this.loguedInUser = user));
   }
 
-  onSubmit() {}
+  onSubmit() {
+    if (this.createTicket.valid) {
+      const formResult = this.createTicket.value;
+      const ticket = {
+        name: formResult.name,
+        description: formResult.description,
+        type: formResult.type,
+        priority: formResult.priority,
+        dateCreated: new Date(),
+        project: this.project._id,
+        author: this.loguedInUser.user._id,
+      };
+      console.log(ticket);
+    }
+  }
 }
