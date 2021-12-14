2a044b2d1c3c5491a9efaa8ad2634ab72b991fcb
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-14 11:05:03 +0100

    Add styles for ticket-members

--- client/src/app/projects/ticket-members/ticket-members.component.ts
+++ client/src/app/projects/ticket-members/ticket-members.component.ts
@@ -3,13 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ticket-members',
   templateUrl: './ticket-members.component.html',
-  styleUrls: ['./ticket-members.component.scss']
+  styleUrls: ['./ticket-members.component.scss'],
 })
 export class TicketMembersComponent implements OnInit {
+  members = [
+    {
+      name: 'Patato',
+      surname: 'Man',
+      role: 'developer',
+      seniority: 'junior',
+    },
+    {
+      name: 'Patato',
+      surname: 'Man',
+      role: 'developer',
+      seniority: 'junior',
+    },
+    {
+      name: 'Patato',
+      surname: 'Man',
+      role: 'developer',
+      seniority: 'junior',
+    },
+    {
+      name: 'Patato',
+      surname: 'Man',
+      role: 'developer',
+      seniority: 'junior',
+    },
+    {
+      name: 'Patato',
+      surname: 'Man',
+      role: 'developer',
+      seniority: 'junior',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
