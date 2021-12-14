b58c9c2748331db1ab9592b8d000582fd1d092cf
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-14 13:05:25 +0100

    Add comments input in ticket comments

--- client/src/app/projects/ticket-comments/ticket-comments.component.ts
+++ client/src/app/projects/ticket-comments/ticket-comments.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ticket-comments',
   templateUrl: './ticket-comments.component.html',
-  styleUrls: ['./ticket-comments.component.scss']
+  styleUrls: ['./ticket-comments.component.scss'],
 })
 export class TicketCommentsComponent implements OnInit {
+  addComment!: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.addComment = new FormGroup({
+      comment: new FormControl('', Validators.required),
+    });
   }
 
+  onSubmit() {
+    if (this.addComment.valid) {
+      console.log(this.addComment.value);
+    }
+  }
 }
