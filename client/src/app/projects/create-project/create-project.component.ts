6916984128c504e9b0f296db9b183647894c11e5
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-15 13:21:22 +0100

    Get project managers dinamically in create project

--- client/src/app/projects/create-project/create-project.component.ts
+++ client/src/app/projects/create-project/create-project.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { NewProject, Project } from 'src/app/models/project.model';
+import { User } from 'src/app/models/user.model';
 import { createProject } from 'src/app/store/projects/projects.actions';
+import { getAllUsers } from 'src/app/store/users/users.actions';
 
 @Component({
   selector: 'app-create-project',
   templateUrl: './create-project.component.html',
   styleUrls: ['./create-project.component.scss'],
 })
-export class CreateProjectComponent implements OnInit {
-  managers = [
+export class CreateProjectComponent implements OnInit, OnDestroy {
+  /* managers = [
     { name: 'bla', _id: '2' },
     { name: 'blu', _id: '3' },
     {
@@ -18,20 +21,28 @@ export class CreateProjectComponent implements OnInit {
       _id: '61aa5d8510c9e6570de7332e',
       hidden: true,
     },
-  ];
+  ]; */
   createProject!: FormGroup;
+  managers!: User[];
+  subscription: Subscription = this.store
+    .select('users')
+    .subscribe(
+      (users) =>
+        (this.managers = users.filter(
+          (user) => user.role === 'project-manager'
+        ))
+    );
 
-  constructor(private store: Store<{ projects: Project[] }>) {}
+  constructor(private store: Store<{ projects: Project[]; users: User[] }>) {}
 
   ngOnInit() {
+    this.store.dispatch(getAllUsers());
+
     this.createProject = new FormGroup(
       {
         name: new FormControl('', Validators.required),
         description: new FormControl('', Validators.required),
-        projectManager: new FormControl(
-          '61aa5d8510c9e6570de7332e',
-          Validators.required
-        ),
+        projectManager: new FormControl('', Validators.required),
       },
       { updateOn: 'blur' }
     );
@@ -41,12 +52,16 @@ export class CreateProjectComponent implements OnInit {
     if (this.createProject.valid) {
       const formResult = this.createProject.value;
       const project: NewProject = {
-        name: formResult.name,
-        description: formResult.description,
+        name: formResult.name.trim(),
+        description: formResult.description.trim(),
         dateCreated: new Date(),
         members: { projectManager: formResult.projectManager },
       };
       this.store.dispatch(createProject({ project }));
     }
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
