57d887634789ef974b2c8971a2ea43da40aaae04
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-12-16 07:18:53 +0100

    Add wildcard route in app module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -23,6 +23,7 @@ const routes: Routes = [
     loadChildren: () =>
       import('./login/login.module').then((m) => m.LoginModule),
   },
+  { path: '**', redirectTo: 'projects' },
 ];
 
 @NgModule({
